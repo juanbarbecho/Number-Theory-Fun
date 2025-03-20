d0541c30c4ce2863410cf6f740febd0d280bd321
juanbarbecho <juan.bar310@gmail.com>
2025-03-19 23:47:59 -0400

    Add files via upload
    Euclidean Algorithm for finding Greatest Common Divisor

---
+++ Program 3-Euclidean Algorithm.py
@@ -0,0 +1,17 @@
+#Computer Program 3
+#Juan Barbecho
+#Euclidean Algorithm to find the GCD of two numbers
+print("Give me two numbers and I will find their GCD")
+A=a=int(input("What is the bigger number?"))
+B=b=int(input("What is the smaller number?"))
+r=1
+count=0
+while r!=0:
+    print("GCD of",a,"and",b,"=")
+    r=a%b
+    a=b
+    b=r
+    count+=1
+print("The GCD of",A,"and",B,"is",a)
+print("The Algorithm repeated itself",count,"times to find the GCD")
+    
